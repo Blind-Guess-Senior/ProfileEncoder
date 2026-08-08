1e855c5c9ac0e3bbd16fe8f3341a4a7787c71d40
Blind-Guess-Senior <94767867+Blind-Guess-Senior@users.noreply.github.com>
2026-08-08 20:25:42 +0800

    feat: logger

--- src/ProfileEncoder.cpp
+++ src/ProfileEncoder.cpp
@@ -2,14 +2,98 @@
 import std;
 import std.compat;
 import argparse;
-import fs_utils;
+import logger;
 
-using std::print;
-using std::println;
+// using std::println;
 using std::string;
+using std::string_view;
 using std::vector;
 
 namespace stdfs = std::filesystem;
+
+constexpr string_view LOG_FOLDER = "log";
+constexpr string_view PROFILE_FOLDER = "profiles";
+
+bool is_stat_disabled;
+// Logger* logger;
+
+static void configure_argument_parser(argparse::ArgumentParser& parser);
+[[nodiscard]] stdfs::path normalized_path(const std::string_view path_raw);
+
+int main(const int argc, const char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+
+    argparse::ArgumentParser argumentParser{"ProfileEncoder", "0.1.0"};
+    configure_argument_parser(argumentParser);
+
+    const vector<string> arguments(argv, argv + argc);
+    argumentParser.parse_args(arguments);
+
+    const auto profileRaw = argumentParser.get<string>("--profile");
+    auto inputs = argumentParser.get<vector<string>>("--input");
+    is_stat_disabled = argumentParser.get<bool>("--no-stat");
+    const bool isLogDisabled = argumentParser.get<bool>("--no-log");
+
+    auto cwd = stdfs::current_path() / "";
+    auto exePath = stdfs::weakly_canonical(stdfs::path{argv[0]}).parent_path(); // https://stackoverflow.com/a/55579815
+    auto profilePath = exePath / PROFILE_FOLDER / "";
+    auto logPath = exePath / LOG_FOLDER / "";
+
+    Logger logger{logPath, isLogDisabled};
+
+    // Log toggle info
+    if (is_stat_disabled) {
+        logger.Log("Statistics disabled. No statistics info will be output.");
+    }
+    if (isLogDisabled) {
+        logger.Log("Console log disabled.");
+    }
+
+    // Process profile
+    bool isLocalProfile = false;
+    if (profileRaw[0] == '.') {
+        isLocalProfile = true;
+    }
+    stdfs::path targetProfileFile;
+    if (isLocalProfile) {
+        targetProfileFile = cwd / stdfs::path{profileRaw.substr(1) + ".txt"};
+    }
+    else {
+        targetProfileFile = profilePath / stdfs::path{profileRaw + ".txt"};
+    }
+
+    if (!stdfs::exists(targetProfileFile)) {
+        logger.Log("Position {} does not contain given profile '{}'.",
+                   isLocalProfile ? cwd.generic_string() : profilePath.generic_string(),
+                   isLocalProfile ? profileRaw.substr(1) : profileRaw);
+        return 0;
+    }
+    if (!stdfs::is_regular_file(targetProfileFile)) {
+        logger.Log("Profile '{}' in position {} is not a valid file.", profileRaw,
+                   isLocalProfile ? cwd.generic_string() : profilePath.generic_string());
+        return 0;
+    }
+
+    // Read profile
+    std::ifstream profileFile{targetProfileFile};
+    if (!profileFile.is_open()) {
+        std::cerr << "Fatal error: cannot open profile file '" << targetProfileFile.generic_string() << "'.\n";
+        logger.Log("Fatal error: cannot open profile file '{}'", targetProfileFile.generic_string());
+        return 0;
+    }
+    vector<string> profileParams{};
+    string param;
+    while (profileFile >> param) {
+        profileParams.push_back(param);
+    }
+
+    logger.Log("Encoding with profile params {}", profileParams);
+
+    auto testPath1 = normalized_path(inputs[0]);
+
+    return 0;
+}
 
 static void configure_argument_parser(argparse::ArgumentParser& parser)
 {
@@ -24,31 +108,11 @@ static void configure_argument_parser(argparse::ArgumentParser& parser)
         .help("Input files. Can be a single file or a txt that every single line refers to a file. Multiple input arg "
               "will be composed.");
     parser.add_argument("--no-stat").flag().help("Turn off statistics info.");
+    parser.add_argument("--no-log").flag().help("Turn off command line log output. File log will still preserved.");
 }
 
-
-int main(const int argc, const char* argv[])
+[[nodiscard]] stdfs::path normalized_path(const std::string_view path_raw)
 {
-    std::ios_base::sync_with_stdio(false);
-
-    argparse::ArgumentParser argumentParser{"ProfileEncoder", "0.1.0"};
-    configure_argument_parser(argumentParser);
-
-    const vector<string> arguments(argv, argv + argc);
-    argumentParser.parse_args(arguments);
-
-    const auto profile = argumentParser.get<string>("--profile");
-    auto inputs = argumentParser.get<vector<string>>("--input");
-    const auto isStatDisabled = argumentParser.get<bool>("--no-stat");
-
-    if (isStatDisabled) {
-        println("Statistics disabled. No statistics info will be output.");
-    }
-
-    auto testPath1 = normalize_path(inputs[0]);
-
-    println("string: {}", testPath1.string());
-    println("generic string: {}", testPath1.generic_string());
-
-    return 0;
+    stdfs::path path{path_raw};
+    return stdfs::weakly_canonical(path);
 }
---
+++ src/logger.ixx
@@ -0,0 +1,55 @@
+export module logger;
+
+import std;
+
+export class Logger
+{
+public:
+    explicit Logger(const std::filesystem::path& log_folder, const bool console_output_disabled = true) :
+        m_consoleOutputDisabled(console_output_disabled)
+    {
+        const auto now = std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now());
+        const auto timePoint = std::chrono::current_zone()->to_local(now);
+        const auto timeString = std::format("{:%Y%m%d%H%M%S}", timePoint);
+
+        m_logFilePath = log_folder / (timeString + ".log");
+
+        if (const std::filesystem::path parentDir = m_logFilePath.parent_path(); !std::filesystem::exists(parentDir)) {
+            if (std::error_code ec; !std::filesystem::create_directories(parentDir, ec)) {
+                if (ec) {
+                    std::cerr << "Fatal error: cannot create log dir." << ec.message() << "\n";
+                    throw std::runtime_error("Failed to create log folder.");
+                }
+            }
+        }
+
+        m_logFile.open(m_logFilePath);
+        if (!m_logFile.is_open()) {
+            std::cerr << "Fatal error: cannot open log file.\n";
+            throw std::runtime_error("Failed to open log file.");
+        }
+    }
+
+    template <typename... Args>
+    void Log(std::format_string<Args...> fmt, Args&&... args)
+    {
+        Log(std::format(fmt, std::forward<Args>(args)...));
+    }
+
+    void Log(std::string_view log)
+    {
+        const auto now = std::chrono::floor<std::chrono::milliseconds>(std::chrono::system_clock::now());
+        const auto timePoint = std::chrono::current_zone()->to_local(now);
+        const auto timeString = "[" + std::format("{:%F %T}", timePoint) + "] ";
+
+        if (!m_consoleOutputDisabled) {
+            std::println("{}", log);
+        }
+        m_logFile << timeString << log << "\n";
+    }
+
+private:
+    std::filesystem::path m_logFilePath;
+    std::ofstream m_logFile;
+    bool m_consoleOutputDisabled;
+};
